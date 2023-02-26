ec52443e3f66e8de1bafd51152e2d941018bf580
saeki-taku <63911366+saeki-taku@users.noreply.github.com>
2023-02-26 21:30:03 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,5 +7,5 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <div style={{ height: "300px" }}></div>;
+    return <div style={{ height: "350px" }}></div>;
 }
