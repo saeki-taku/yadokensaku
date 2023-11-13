bccf4d5f378cbca38504be78932dc50b078e575e
saeki-taku <63911366+saeki-taku@users.noreply.github.com>
2023-11-13 23:26:46 +0900

    firebase　準備

--- src/pages/api/hotel.ts
+++ src/pages/api/hotel.ts
@@ -4,6 +4,3 @@ export const Hotel = async (hotel_no: number) => {
     const res = await fetch(`${BASE_URL}?format=json&hotelNo=${hotel_no}&responseType=large&applicationId=1081252385066817375`);
     return res.json();
 };
-
-
-// https://app.rakuten.co.jp/services/api/Travel/HotelDetailSearch/20170426?format=json&hotelNo=806&responseType=large&applicationId=e06e2a5afcf14b52139c1fb6c58e9dbc
--- src/pages/api/ranking.ts
+++ src/pages/api/ranking.ts
@@ -13,6 +13,6 @@ const BASE_URL = "https://app.rakuten.co.jp/services/api/Travel/HotelRanking/201
 //     return res.json();
 // };
 export const Ranking = async (genre: any) => {
-    const res = await fetch(`${BASE_URL}?format=json&genre=${genre}&applicationId=1081252385066817375`);
+    const res = await fetch(`${BASE_URL}?format=json&genre=${genre}&applicationId=${process.env.RAKUTEN_API_KEY}`);
     return res.json();
 };
