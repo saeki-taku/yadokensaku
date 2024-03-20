cbf5854be9200efb8e2db8b5792416cc19813388
saeki-taku <63911366+saeki-taku@users.noreply.github.com>
2024-03-20 13:28:58 +0900

    fix

--- src/components/Mypage/MypageFavoriteList/index.tsx
+++ src/components/Mypage/MypageFavoriteList/index.tsx
@@ -11,7 +11,7 @@ import ReactStarsRating from "react-awesome-stars-rating";
 import { getFavoriteHotelIds } from "../../../utils/myhotel";
 // firebase
 import { doc, updateDoc, arrayRemove } from "firebase/firestore";
-import { db } from "@/lib/firebaseConfig";
+import { db } from "@/lib/firebaseConfig.js";
 import "firebase/firestore";
 // zustand
 import { useUserStore, useFavoriteStore } from "@/hooks/useUserStore";
